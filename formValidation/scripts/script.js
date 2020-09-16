464cab0ee1ca74c4a6888118374aceb020a8f5a5
M3dython <samuelcbba@gmail.com>
2020-09-16 04:39:30 -0300

    form validation comments

--- formValidation/scripts/script.js
+++ formValidation/scripts/script.js
@@ -1,49 +1,71 @@
-const form = document.getElementById("form");
-const username = document.getElementById("username");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const password2 = document.getElementById("password2");
+// UNIVERSAL CONSTANTS AND VARIABLES
+// get all elements with the id of form in the html
+const form = document.getElementById('form');
+// gets all elements with the id of username
+const username = document.getElementById('username');
+// gets all elements with the id of email
+const email = document.getElementById('email');
+// gets all elements with the id of password
+const password = document.getElementById('password');
+// gets all elements with the id of password2
+const password2 = document.getElementById('password2');
+
+// FUNCTIONS
 
 // Show input error message
+// has the parameter input and message
 function showError(input, message) {
+  // gives the formControl constant the value of the input parameter
   const formControl = input.parentElement;
-  formControl.className = "form-control error";
-  const small = formControl.querySelector("small");
+  // changes the formControl class name in the html
+  formControl.className = 'form-control error';
+  // gives the constant small the constant form control with the output of the querySelector function, the function invokes the small variable (as a parameter)
+  const small = formControl.querySelector('small');
+  // makes the innertext of the small constant equals the message parameter
   small.innerText = message;
 }
 
-// Show sucess outline
-function showSucess(input) {
+// Show success outline, with input as a parameter
+function showSuccess(input) {
+  //parent element get the node name of the parent element, it will be added to the form control, and change its class to success
   const formControl = input.parentElement;
-  formControl.className = "form-control sucess";
+  formControl.className = 'form-control sucess';
 }
 
 // Check email is valid
 function checkEmail(input) {
+  //re is used as variables to be tested if the email format is valid
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  // test method tests for a match in "re"
+  //trim method removes whitespace from both sides
   if (re.test(input.value.trim())) {
-    showSucess(input);
+    showSuccess(input);
   } else {
-    showError(input, "Email is not valid");
+    showError(input, 'Email is not valid');
   }
 }
 
 // Check required fields
+//the input of the checkRequired function must be an array
 function checkRequired(inputArr) {
+  // for each element of the array, check the fuction with the input as parameter
   inputArr.forEach(function (input) {
-    if (input.value.trim() === "") {
+    //if the input is empty show that the field name is required to be filled
+    if (input.value.trim() === '') {
       showError(input, `${getFieldName(input)} is required`);
     } else {
-      showSucess(input);
+      showSuccess(input);
     }
   });
 }
 
 // Check input length
+// input, min and max will be this function parameter
 function checkLength(input, min, max) {
   if (input.value.length < min) {
     showError(
       input,
+      //the field name is floating, so this function can be used in more then one place
       `${getFieldName(input)} must be at least ${min} characters.`
     );
   } else if (input.value.length > max) {
@@ -52,29 +74,38 @@ function checkLength(input, min, max) {
       `${getFieldName(input)} must be less than ${max} characters`
     );
   } else {
-    showSucess(input);
+    showSuccess(input);
   }
 }
 
 // Get Field name
 function getFieldName(input) {
+  //makes the first letter uppercase. then adds the rest of the string
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 // Check paswords match
+
 function checkPasswordsMatch(input1, input2) {
+  // if the value of the first is different of the value of the second show error
   if (input1.value !== input2.value) {
     showError(input2, `Passwords do not match`);
   }
 }
 
-// Event listener
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
+// EVENT LINSTENERS
+// the event parameter has different methods; prevent default wont let it work as default
+//after submit do the following functions
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  //create the array checkRequired and gives the values from the html input
   checkRequired([username, email, password, password2]);
+  //call the function checking the min,max lenghts of the first parameter
   checkLength(username, 3, 15);
   checkLength(password, 6, 25);
   checkLength(password2, 6, 25);
+  //checks if the email is valid
   checkEmail(email);
+  // checks if the passwords match
   checkPasswordsMatch(password, password2);
 });
